3968a3c8c8d7838b58a2f647de3a8b59d5a871f3
marekhuckmann <marek.hueckmann@gmail.com>
2017-11-16 16:19:54 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@ $(document).ready(function () {
     $('.swipebox').swipebox();
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+{
+    if (document.querySelector(".materials") !== null) {
+        var x = document.querySelector(".materials").clientHeight;
+
+        var heightSetter = document.createElement('style');
+        heightSetter.type = 'text/css';
+
+        if (x <= 200) {
+            x *= 2;
+            heightSetter.innerHTML = '.materials::before {height: ' + x + 'px;}';
+            document.getElementsByTagName('head')[0].appendChild(heightSetter);
+        } else if (x <= 300) {
+            x *= 1.816;
+            heightSetter.innerHTML = '.materials::before {height: ' + x + 'px;}';
+            document.getElementsByTagName('head')[0].appendChild(heightSetter);
+        }
+    }
+}
 /*!
   * Bootstrap v4.0.0-beta.2 (https://getbootstrap.com)
   * Copyright 2011-2017 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
